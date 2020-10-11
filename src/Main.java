8cc2c7e65d8d2214e9f980591e0c4b347d4ae17b
unknown <aleksey_nikeshin@mail.ru>
2020-10-11 22:06:26 +0300

    fixed #1

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,29 @@ public class Main {
     public static void main(String[] args) {
         double percent = 1;
         int regularExpenses = 290000;
-        double cashbackRegularExpenses = percent * regularExpenses/100;
-        double limit = 3000;
-        if (cashbackRegularExpenses>limit){cashbackRegularExpenses=limit;}
+        double cashbackRegularExpenses = percent * regularExpenses / 100;
+        int limit = 3000;
+        if (cashbackRegularExpenses > limit) {
+            cashbackRegularExpenses = limit;
+        }
 
         double percentElevatedCashback = 5;
         int buys = 15000;
-        double cashbackBuys = percentElevatedCashback * buys/100;
-        if (cashbackBuys>limit){cashbackBuys=limit;}
+        double cashbackBuys = percentElevatedCashback * buys / 100;
+        if (cashbackBuys > limit) {
+            cashbackBuys = limit;
+        }
 
         double specialPercent = 3;
-        int SpecialBuys = 5000;
-        double specialCashback = specialPercent * SpecialBuys/100;
-        if (specialCashback>limit){specialPercent=limit;}
+        int specialBuys = 5000;
+        double specialCashback = specialPercent * specialBuys / 100;
+        if (specialCashback > limit) {
+            specialPercent = limit;
+        }
         double total = cashbackRegularExpenses + cashbackRegularExpenses + specialCashback;
-
+        if (total > limit) {
+            total = limit;
+        }
         System.out.println(total);
     }
-    }
+}
